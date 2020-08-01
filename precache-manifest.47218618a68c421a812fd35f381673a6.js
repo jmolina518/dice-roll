278862e81688ef56a8ce5b6c4b266b0ae77b8320
jmolina518 <jmolina518@gmail.com>
2020-08-01 10:12:21 -0500

    Updates

---
+++ precache-manifest.47218618a68c421a812fd35f381673a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f210f596515e4191980b1112c8bb47f9",
+    "url": "/dice-roll/index.html"
+  },
+  {
+    "revision": "19fc71aca1446f9fe47d",
+    "url": "/dice-roll/static/css/main.5c0d4d73.chunk.css"
+  },
+  {
+    "revision": "944b52c28e42565d7033",
+    "url": "/dice-roll/static/js/2.98856e30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dice-roll/static/js/2.98856e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fc71aca1446f9fe47d",
+    "url": "/dice-roll/static/js/main.b047021e.chunk.js"
+  },
+  {
+    "revision": "373c2847e37adb01b6cb",
+    "url": "/dice-roll/static/js/runtime-main.d172bf85.js"
+  }
+]);
